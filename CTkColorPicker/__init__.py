b894c32683111bb8cd249c90ff90d110435aaee9
Akash Bora <89206401+Akascape@users.noreply.github.com>
2023-07-10 12:35:15 +0530

    Fixed a memory issue

--- CTkColorPicker/__init__.py
+++ CTkColorPicker/__init__.py
@@ -5,7 +5,7 @@ This is a modern color picker for customtkinter.
 Homepage: https://github.com/Akascape/CTkColorPicker
 """
 
-__version__ = '0.6.0'
+__version__ = '0.7.0'
 
 from .ctk_color_picker import AskColor
 from .ctk_color_picker_widget import CTkColorPicker
