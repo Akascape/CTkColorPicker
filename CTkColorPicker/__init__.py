2b6b28be118505f3d69d07e7b6548a72786eed02
I Am Iron Man <63475761+iamironman0@users.noreply.github.com>
2024-02-11 19:15:42 +0100

    Version 2112024
    CTkColorPicker Widget - New Release 🎉
    Exciting updates to CTkColorPicker widget!
    
    New Features 🆕
    Logo: A unique logo now identifies the widget.
    Centered Window: The widget window now centers on the screen.
    Color Modes: Choose from light and dark color modes. Returns a list of colors.
    Custom HEX Color: Customize the HEX color for both modes. Returns a list.
    Code Refactoring 🔄
    The code has been refactored into methods for improved performance and easier understanding.
    
    Enjoy these updates and share your feedback!

--- CTkColorPicker/__init__.py
+++ CTkColorPicker/__init__.py
@@ -5,7 +5,7 @@ This is a modern color picker for customtkinter.
 Homepage: https://github.com/Akascape/CTkColorPicker
 """
 
-__version__ = '0.8.0'
+__version__ = '2112024'
 
 from .ctk_color_picker import AskColor
 from .ctk_color_picker_widget import CTkColorPicker
